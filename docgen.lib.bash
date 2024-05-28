968c7f029a59969ca30a2670fe8fc98dc134c1b8
Daniil Shuraev <shuraev.daniil@gmail.com>
2024-05-28 15:20:54 -0400

    feat!: refactoring
    Moved functions into `docgen::` namespace.
    
    Replaced `DOCGEN_DOC_MARK` with `DOCGEN_MARK` and
    changed default to `##`, to fix issues with external formatters
    removing trailing whitespaces. This allows for inclusion of empty
    lines in documentation, which was an issue with defninition lists.
    
    Moved most code into `main` function, used a different check for
    execution/sourcing of the file.
    
    Used `cat` as pager for `man` to get rid of hang-ups waiting for
    user input.

--- docgen.lib.bash
+++ docgen.lib.bash
@@ -1,29 +1,29 @@
 #!/usr/bin/env bash
 
-## % docgen.lib.bash (3) | Version 1.0
+## % docgen.lib.bash (3) | Version 1.1
 ## % Daniil Shuraev
-## % 2024-05-07
+## % 2024-05-28
 
 ## # SYNOPSIS
 ## ```bash
 ## . docgen.lib.bash
-## _docgen_help
-## _docgen_help_raw
+## docgen::help
+## docgen::help_raw
 ## ```
 ## or `docgen.lib.bash --help [--raw]` to view documentation.
 
 ## # DESCRIPTION
 ## These functions will generate documentation based on source code comments.
-## `_docgen_help_raw` will go through the current script and parse all lines
+## `docgen::help_raw` will go through the current script and parse all lines
 ## starting with doc-mark `## ` (note the space) as documentation comments.
 ## These lines will be printed to stdout with the doc-mark stripped.
-## \
+##
 ## The comments will appear in the order they appear in the source code.
-## \
-## `_docgen_help` will do the same, but will convert documentation with `pandoc`
-## and display it with `man`. If either one is not present on the system,
-## `_docgen_help_raw` will be displayed instead.
-## \
+##
+## `docgen::help` will do the same, but will convert documentation with `pandoc`
+## and display it with `man` and `cat`. If either one is not present on the system,
+## `docgen::help_raw` will be displayed instead.
+##
 ## `docgen.lib.bash` can be also executed as a script to view this documentation.
 ## Use `docgen.lib.bash --help [--raw]` to view the docs. Will exit with 1 if
 ## invoked with wrong flags.
@@ -49,53 +49,65 @@
 ## ## Generating Documentation
 ## In order to generate documentation for the script use
 ## ```bash
-## _docgen_help_raw | pandoc -f markdown-blank_before_header -s -t <format>
+## docgen::help_raw | pandoc -f markdown-blank_before_header -s -t <format>
 ## ```
 ## For the list of supported formats, see
 ## [Pandoc documentation](https://pandoc.org/MANUAL.html#general-options).
 
 ## # CONFIGURATION
-## Doc-mark can be configured as needed by setting `DOCGEN_DOC_MARK`
-## environmental variable. The default is `## `.
+## Doc-mark can be configured as needed by setting `DOCGEN_MARK`
+## environmental variable. The default is '##'.
 ## \
 ## The variable must be overridden *after* the script is sourced.
-## Note that the *patterns containing ')' must be avoided*.
+## Note that the *patterns containing ',' must be avoided*.
 
-# doc-mark; should not contain ')' due to regex syntax
-DOCGEN_DOC_MARK="## "
+# doc-mark; should not contain ',' due to regex syntax.
+declare DOCGEN_MARK="##"
 
-# display documentation in raw format; used mainly for piping into something
-_docgen_help_raw() {
-  command cat "$0" |                                 # use `command` in case of user overrides
-    sed -n "\)^[[:space:]]*${DOCGEN_DOC_MARK}.*)p" | # detect marked lines
-    sed 's/^[[:space:]]*//' |                        # strip leading blanks
-    cut -c $((${#DOCGEN_DOC_MARK} + 1))-             # get only part after doc-mark
+## # INTERNAL FUNCTIONS
+## `docgen::help_raw`
+## :  Display documentation in raw format, i.e., stripping the comment docgen
+##    comment marker as well as any of the following spaces.
+##    This is primarily useful for piping into other programs, like pandoc.
+##    See `docgen::help` function for reference usage.
+##
+docgen::help_raw() {
+  command cat "$0" |                             # use `command` in case of user overrides
+    sed -n "\,^[[:space:]]*${DOCGEN_MARK}.*,p" | # detect marked lines
+    sed 's/^[[:space:]]*//' |                    # strip leading blanks
+    cut -c $((${#DOCGEN_MARK} + 1))- |           # get only part after doc-mark
+    sed 's/^[[:space:]]*//'                      # strip leading blanks
 }
 
-# display auto-generated manpage
-_docgen_help() {
-  if [[ -n $(command -v pandoc) && -n $(command -v man) ]]; then
-    _docgen_help_raw | pandoc -f markdown-blank_before_header -s -t man | man -l -
+## `docgen::help`
+## :  Display documentation in a nice format as long as `pandoc`, `man` and `cat`
+##    are available on the system.
+##    `pandoc` is used to convert pandoc-markdown into a man page,
+##    `man` is used for formatting, and
+##    `cat` is used as a pager, to avoid hang-ups.
+##
+docgen::help() {
+  if [[ -n $(command -v pandoc) && -n $(command -v man) && -n $(command -v cat) ]]; then
+    docgen::help_raw | pandoc -f markdown-blank_before_header -s -t man | man -l -P cat -
   else
     echo 'Help message is displayed in raw format.'
     echo 'Install pandoc and man to get better formatting'
     echo
-    _docgen_help_raw
+    docgen::help_raw
   fi
 }
 
-# Do not eval if this file is sourced - only for self-documenting.
-if ! (return 0 2>/dev/null); then
+main() {
   ## # OPTIONS
   while (($# > 0)); do
     case "$1" in
       -h | --help)
-        ## `-h | --help`
+        ## `-h | --help [--raw]`
         ## :  Display this message. Use `--help --raw` to display in raw format.
         if [ "$2" = "--raw" ]; then
-          _docgen_help_raw
+          docgen::help_raw
         else
-          _docgen_help
+          docgen::help
         fi
         exit
         ;;
@@ -105,4 +117,7 @@ if ! (return 0 2>/dev/null); then
         ;;
     esac
   done
-fi
+}
+
+# Do not eval if this file is sourced - only for self-documenting.
+[[ "${0}" == "${BASH_SOURCE[0]}" ]] && main "$@"
